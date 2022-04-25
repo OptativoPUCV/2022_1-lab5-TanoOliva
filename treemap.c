1f71713c2e0f341cf299a6b8c21d45ff65bc8e49
Your Name <you@example.com>
2022-04-24 22:05:01 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -122,13 +122,26 @@ Pair * firstTreeMap(TreeMap * tree) {
 Pair * nextTreeMap(TreeMap * tree) {
     TreeNode * aux = tree->current;
     TreeNode * otroaux = aux->parent;
-    if (aux->right != NULL) aux = minimum(aux->right);
+    if (aux->right != NULL){
+    aux = minimum(aux->right);
     tree->current=aux;
     return aux->pair;
+    }
+    /*
+    while (1){
+        if(otroaux->pair->key>aux->pair->key){
+            return otroaux->pair;
+        }else{
+            otroaux = otroaux->parent;
+        }
+    }
+    */
+    
     while (otroaux != NULL && aux != otroaux->right){
         aux=otroaux;
         otroaux=aux->parent;
     }
     tree->current = otroaux;
     return otroaux->pair;
+    
 }
