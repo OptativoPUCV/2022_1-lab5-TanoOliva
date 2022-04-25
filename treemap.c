e6fa7e29e7e2573fad8cbf1abd828e8349c0a023
Your Name <you@example.com>
2022-04-24 23:21:30 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -46,7 +46,8 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 }
 
 
-void insertTreeMap(TreeMap * tree, void* key, void * value) {    
+void insertTreeMap(TreeMap * tree, void* key, void * value) {   
+    /* 
     Pair * z= (Pair *) malloc (sizeof(Pair));
     z = createTreeNode(key,value);
     TreeNode * y = NULL;
@@ -64,7 +65,7 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
         y->left->pair = z;
     }
     return;
-    
+    */
 }
 
 TreeNode * minimum(TreeNode * x){
